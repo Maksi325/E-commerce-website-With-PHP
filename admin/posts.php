f4fd8191b76392561445d22051e35fc92559f9a6
Maksi325 <maksimmajmari12@gmail.com>
2021-11-28 01:43:37 +0100

    adding add post option on admin section

---
+++ admin/posts.php
@@ -0,0 +1,49 @@
+<?php   include "includes/header.php";  ?>
+    <div id="wrapper">
+        <!-- Navigation -->
+        <?php   include "includes/navigation.php";  ?>
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                       
+                        <h1 class="page-header">
+                            Welcome to admin
+                            <small>Author</small>
+                        </h1>
+                        <?php
+                        if(isset($_GET['source'])){
+                            $source = $_GET['source'];
+                        }else {
+                            $source = 0;
+                        }
+                        
+                        switch($source){
+                            case '1':
+                                include "includes/add_post.php";
+                                break;
+                            case '2':
+                                echo "Nice 2";
+                                break;
+                            case '3':
+                                echo "Nice 3";
+                                break;
+                            case '4':
+                                echo "Nice 4";
+                                break;
+                            default:
+                                include "includes/view_all_posts.php";
+                                break;
+                        }
+                        
+                          ?>
+                        
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+        <!-- /#page-wrapper -->
+        <?php   include "includes/footer.php";   ?>
